3e6791cfa8dc6953cc8c8f4ca48a749802f5fb43
Keshav <keshav.poha@gmail.com>
2026-05-14 16:33:43 +0530

    fix: use DSN method to correctly format db port

--- internal/database/database.go
+++ internal/database/database.go
@@ -11,10 +11,7 @@ import (
 )
 
 func Connect(cfg *config.Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBSSLMode)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
