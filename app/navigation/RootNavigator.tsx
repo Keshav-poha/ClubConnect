46177f5d4dadbf1b93bf11cec70ed33cbcc08519
Keshav <keshav.poha@gmail.com>
2026-05-22 21:16:05 +0530

    feat(navigation): integrate EventDetailScreen into RootNavigator

--- app/navigation/RootNavigator.tsx
+++ app/navigation/RootNavigator.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TabNavigator } from './TabNavigator';
-// We will import EventDetailScreen here in the future
+import { EventDetailScreen } from '@/screens/EventDetailScreen';
+import { RootStackParamList } from './types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="MainTabs" component={TabNavigator} />
-      {/* <Stack.Screen name="EventDetail" component={EventDetailScreen} /> */}
+      <Stack.Screen name="EventDetail" component={EventDetailScreen} />
     </Stack.Navigator>
   );
 };
---
+++ app/navigation/types.ts
@@ -0,0 +1,6 @@
+import { Event } from '@/types';
+
+export type RootStackParamList = {
+  MainTabs: undefined;
+  EventDetail: { event: Event };
+};
